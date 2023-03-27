57077e26734f70c32e1dcc018a11aba4415a25d8
Gavin <me@gkasdorf.com>
2023-03-26 20:17:10 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# apple music jwt generator
+ A simple tool to generate Apple Music JWT tokens
---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const inquirer = require("inquirer");
+
+const questions = [
+    {
+        type: "input",
+        name: "teamId",
+        message: "Enter your Apple Developer Team ID: "
+    },
+    {
+        type: "input",
+        name: "keyId",
+        message: "Enter your Apple Music key ID: "
+    },
+    {
+        type: "input",
+        name: "keyPath",
+        message: "Enter the path to your Apple Music key: "
+    }
+];
+
+const main = () => {
+    inquirer.prompt(questions).then(answers => {
+        const privateKey = fs.readFileSync(answers.keyPath).toString();
+
+        const jwtToken = jwt.sign({}, privateKey, {
+            algorithm: "ES256",
+            expiresIn: "180d",
+            issuer: answers.teamId,
+            header: {
+                alg: "ES256",
+                kid: answers.keyId
+            }
+        });
+
+        console.log(jwtToken);
+    });
+}
+
+main();
